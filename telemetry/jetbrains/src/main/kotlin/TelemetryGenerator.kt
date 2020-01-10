d4e96a218b86a040c79af4cf681bb108ea17884d
Hunter Werlla <werlla@amazon.com>
2020-01-10 15:34:26 -0800

    Put type in object

--- telemetry/jetbrains/src/main/kotlin/TelemetryGenerator.kt
+++ telemetry/jetbrains/src/main/kotlin/TelemetryGenerator.kt
@@ -44,45 +44,56 @@ fun generateTelemetryEnumTypes(output: FileSpec.Builder, items: List<TelemetryMe
     }
 }
 
-fun generateRecordFunctions(output: FileSpec.Builder, items: TelemetryDefinition) {
-    items.metrics.forEach { metric ->
-        val functionBuilder = FunSpec.builder("record${metric.name.snakeCaseToCamelCase()}")
-        // generate parameters
-        val projectParameter = ClassName("com.intellij.openapi.project", "Project").copy(nullable = true)
-        val valueParameter = com.squareup.kotlinpoet.DOUBLE
-        val additionalParameters = metric.metadata?.map { metadata ->
-            val telemetryMetricType =
-                items.types.find { it.name == metadata.type } ?: throw IllegalStateException("Type ${metadata.type} on ${metric.name} not found in types!")
-            val typeName = if (telemetryMetricType.allowedValues != null) {
-                ClassName(PACKAGE_NAME, telemetryMetricType.name.filterInvalidCharacters())
-            } else {
-                telemetryMetricType.type?.getTypeFromType() ?: com.squareup.kotlinpoet.STRING
-            }.copy(nullable = metadata.required ?: false)
-            ParameterSpec(telemetryMetricType.name.filterInvalidCharacters(), typeName)
-        } ?: listOf()
-        functionBuilder
-            .addParameter("project", projectParameter)
-            .addParameter(ParameterSpec.builder("value", valueParameter).defaultValue("1.0").build())
-            .addParameters(additionalParameters)
-        // generate body
-        val unit = MemberName("software.amazon.awssdk.services.toolkittelemetry.model", "Unit")
-        functionBuilder
-            .addStatement("TelemetryService.getInstance().record(project) { ")
-            .addStatement("datum(%S) {", metric.name)
-            .addStatement("unit(%M.${(metric.unit ?: MetricUnit.NONE).name})", unit)
-            .addStatement("value(value)")
-        metric.metadata?.forEach {
-            functionBuilder.addStatement("metadata(%S, %L.toString())", it.type, it.type)
-        }
-        functionBuilder.addStatement("}}")
+//fun generateNamespaces(metrics: List<Metric>): List<TypeSpec.Builder> = metrics.map { it.name.split("_").first().toLowerCase().capitalize() }.map {  }
 
-        output.addFunction(functionBuilder.build())
-    }
+fun generateRecordFunctions(output: FileSpec.Builder, items: TelemetryDefinition) {
+    items
+        .metrics
+        .sortedBy { it.name }
+        .groupBy { it.name.split("_").first().toLowerCase() }
+        .forEach { metrics: Map.Entry<String, List<Metric>> ->
+            val namespace = TypeSpec.objectBuilder("${metrics.key.capitalize()}Telemetry")
+            metrics.value.forEach { metric ->
+                val functionBuilder = FunSpec.builder("record${metric.name.split("_")[1].capitalize()}")
+                // generate parameters
+                val projectParameter = ClassName("com.intellij.openapi.project", "Project").copy(nullable = true)
+                val valueParameter = com.squareup.kotlinpoet.DOUBLE
+                val additionalParameters = metric.metadata?.map { metadata ->
+                    val telemetryMetricType =
+                        items.types.find { it.name == metadata.type }
+                            ?: throw IllegalStateException("Type ${metadata.type} on ${metric.name} not found in types!")
+                    val typeName = if (telemetryMetricType.allowedValues != null) {
+                        ClassName(PACKAGE_NAME, telemetryMetricType.name.filterInvalidCharacters())
+                    } else {
+                        telemetryMetricType.type?.getTypeFromType() ?: com.squareup.kotlinpoet.STRING
+                    }.copy(nullable = metadata.required ?: false)
+                    ParameterSpec(telemetryMetricType.name.filterInvalidCharacters(), typeName)
+                } ?: listOf()
+                functionBuilder
+                    .addParameter("project", projectParameter)
+                    .addParameter(ParameterSpec.builder("value", valueParameter).defaultValue("1.0").build())
+                    .addParameters(additionalParameters)
+                // generate body
+                val unit = MemberName("software.amazon.awssdk.services.toolkittelemetry.model", "Unit")
+                functionBuilder
+                    .addStatement("TelemetryService.getInstance().record(project) { ")
+                    .addStatement("datum(%S) {", metric.name)
+                    .addStatement("unit(%M.${(metric.unit ?: MetricUnit.NONE).name})", unit)
+                    .addStatement("value(value)")
+                metric.metadata?.forEach {
+                    functionBuilder.addStatement("metadata(%S, %L.toString())", it.type, it.type)
+                }
+                functionBuilder.addStatement("}}")
+                namespace.addFunction(functionBuilder.build())
+            }
+            output.addType(namespace.build())
+        }
 }
 
 fun main(vararg args: String) {
     val telemetry = parse()
     val output = FileSpec.builder(PACKAGE_NAME, "HelloWorld")
+    //val namespaces = generateNamespaces(telemetry.metrics)
     addImports(output)
     generateTelemetryEnumTypes(output, telemetry.types)
     generateRecordFunctions(output, telemetry)
