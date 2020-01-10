86a451077c048e0efa7ed5da864916cdbb7d699f
Hunter Werlla <werlla@amazon.com>
2020-01-10 13:20:15 -0800

    start generating functions

--- telemetry/jetbrains/src/main/kotlin/TelemetryGenerator.kt
+++ telemetry/jetbrains/src/main/kotlin/TelemetryGenerator.kt
@@ -1,6 +1,12 @@
+import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.TypeSpec
+
+fun addImports(output: FileSpec.Builder) {
+    output.addImport("software.aws.toolkits.jetbrains.services", "telemetry")
+    output.addImport("import com.intellij.openapi.project", "Project")
+}
 
 fun generateTelemetryEnumTypes(output: FileSpec.Builder, items: List<TelemetryMetricType>) {
     items.forEach {
@@ -26,13 +32,18 @@ fun generateTelemetryEnumTypes(output: FileSpec.Builder, items: List<TelemetryMe
 
 fun generateRecordFunctions(output: FileSpec.Builder, items: TelemetryDefinition) {
     items.metrics.forEach {
-        
+        val thing = ClassName("com.intellij.openapi.project", "Project").copy(nullable = true)
+        val functionBuilder = FunSpec
+            .builder("abc").addParameter("project", thing)
+            .build()
+        output.addFunction(functionBuilder)
     }
 }
 
 fun main(vararg args: String) {
     val telemetry = parse()
     val output = FileSpec.builder("software.amazon.toolkits.telemetry", "HelloWorld")
+    addImports(output)
     generateTelemetryEnumTypes(output, telemetry.types)
     generateRecordFunctions(output, telemetry)
     output.build().writeTo(System.out)
